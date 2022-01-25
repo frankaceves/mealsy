cc3da421d77b5f30df164eef45353434a051f9ac
Frank Aceves <frankaceves@gmail.com>
2022-01-24 21:41:53 -0800

    change Category & Categories structs to Decodable

--- mealsy/Category.swift
+++ mealsy/Category.swift
@@ -5,10 +5,10 @@
 //  Created by Frank Aceves on 1/24/22.
 //
 
-struct Categories {
+struct Categories: Decodable {
     let categories: [Category]
 }
-struct Category {
+struct Category: Decodable {
     let idCategory: String
     let strCategory: String
     let strCategoryThumb: String
