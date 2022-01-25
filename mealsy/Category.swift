92fff4e5a5917a0802b98362bbec7f9e00f142a2
Frank Aceves <frankaceves@gmail.com>
2022-01-24 18:06:32 -0800

    add Category struct

---
+++ mealsy/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  mealsy
+//
+//  Created by Frank Aceves on 1/24/22.
+//
+
+struct Category {
+    let idCategory: String
+    let strCategory: String
+    let strCategoryThumb: String
+    let strCategoryDescription: String
+}
