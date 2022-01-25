f7129cf93c7ad35b4c9fac7f5225e2c11cbd3e5d
Frank Aceves <frankaceves@gmail.com>
2022-01-24 18:35:10 -0800

    set up Categories struct

--- mealsy/Category.swift
+++ mealsy/Category.swift
@@ -5,9 +5,13 @@
 //  Created by Frank Aceves on 1/24/22.
 //
 
+struct Categories {
+    let categories: [Category]
+}
 struct Category {
     let idCategory: String
     let strCategory: String
     let strCategoryThumb: String
     let strCategoryDescription: String
 }
+
