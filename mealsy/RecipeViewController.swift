7b1b09945c155df8b53a0e1546e5d00ae7f08aff
Frank Aceves <frankaceves@gmail.com>
2022-03-30 16:34:17 -0700

    download recipe information to Recipe View Controller

--- mealsy/RecipeViewController.swift
+++ mealsy/RecipeViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 class RecipeViewController: UIViewController {
     var recipeID: String?
     var recipeName: String?
-    
+    var recipeVM = RecipeViewModel()
     @IBOutlet weak var recipeHeaderLabel: UILabel!
     @IBOutlet weak var ingredientsLabel: UILabel!
     @IBOutlet weak var instructionsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        recipeHeaderLabel.text = "How To Make \(recipeName!)"
+        loadViews()
+    }
+    func loadViews() {
+        guard let id = recipeID, let name = recipeName else {
+            return
+        }
+        recipeHeaderLabel.text = "How To Make \(name)"
+        recipeVM.downloadRecipe(id: id) { [unowned self] success, recipe in
+            guard success else {
+                return
+            }
+            guard let recipe = recipe else {
+                return
+            }
+            DispatchQueue.main.async {
+                instructionsLabel.text = recipe.strInstructions
+            }
+        }
     }
 }
