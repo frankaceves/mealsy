98f61ed4aa267b1c43b5a2b9924ede8e699dde17
Frank Aceves <frankaceves@gmail.com>
2022-03-30 11:31:31 -0700

    add Meal model

---
+++ mealsy/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  mealsy
+//
+//  Created by Frank Aceves on 3/30/22.
+//
+
+import Foundation
+struct Meal {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
